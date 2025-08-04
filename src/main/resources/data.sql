532db024a5c75fc7c030bc9238d006008bd70e9f
shaeque16 <shaeque16@gmail.com>
2025-08-04 20:07:34 +0530

    changes in properties file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO doctors (name, email, phone, specialization, available_from, available_to) values
+('Aman', 'aman67@gmai', '9871079090', 'Cardiology', '10:00:00', '19:00:00'),
+('Sahil', 'sahil65@gmail.com', '9874579111', 'Physician', '09:00:00', '17:00:00');
+
+
+
+INSERT INTO patients (name, age, email, phone, gender) values
+('Rahul','23','rahul@gmail.com', '7638374930', 'Male' ),
+('Steven', '30', 'steven12@gmail.com', '8768654421', 'Male');
+
+
+
+
